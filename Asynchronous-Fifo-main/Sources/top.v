0e0d37dcd1bb541adb05eb1f4e485224e63da1b3
Raza Abbas <raza13abbas@gmail.com>
2026-05-17 20:46:56 +0530

    Update top.v

--- Asynchronous-Fifo-main/Sources/top.v
+++ Asynchronous-Fifo-main/Sources/top.v
@@ -1,109 +1,160 @@
-module afifo#(parameter DEPTH =8 ,parameter WIDTH=4)(
-input wclk,
-input rclk,
-input wen,
-input ren,
-input rst,
-input [DEPTH-1:0]data,
-  output reg [DEPTH-1:0]out, 
-output    full,
-output   empty
+module async_fifo_16x8 (
+
+    input  wire        wclk,
+    input  wire        rclk,
+    input  wire        rst,
+
+    input  wire        wen,
+    input  wire        ren,
+
+    input  wire [7:0] data_in,
+
+    output reg  [7:0] data_out,
+
+    output wire       full,
+    output wire       empty
 );
-  
-  integer i;
-  
-  reg [DEPTH-1:0]mem[0:WIDTH-1];
-  reg [$clog2(WIDTH):0]wptr;
-  reg [$clog2(WIDTH):0]rptr;
-  reg [$clog2(WIDTH):0]wptr1;
-  reg [$clog2(WIDTH):0]rptr1;
-  reg [$clog2(WIDTH):0]wptr2;
-  reg [$clog2(WIDTH):0]rptr2;
-  reg [$clog2(WIDTH):0]f1;
-  reg [$clog2(WIDTH):0]f2;
-  reg [$clog2(WIDTH):0]bin;
-  reg [$clog2(WIDTH):0]wbin;
-  
-  
-  always@(posedge wclk)
-    begin
-    if(rst)begin 
-    out<=0;
-      wptr<=0;
-      rptr<=0;
-      wptr1<=0;
-       rptr1<=0;
-      wptr2<=0;
-       rptr2<=0;
-      f1<=0;
-      f2<=0;
-    end
-  else
-    begin
-      if(wen && !full)begin
-        mem[wptr[$clog2(WIDTH)-1:0]]<=data;
-      wptr<=wptr+1;
-        wptr1<=((wptr+1)>>1)^(wptr+1); 
-      end
-   
-    end
-    end
- 
-  always@(posedge rclk)
-    begin
-    if(rst)begin 
-    out<=0;
-      wptr<=0;
-       rptr<=0;
-       wptr1<=0;
-       rptr1<=0;
-      wptr2<=0;
-       rptr2<=0;
-      f1<=0;
-      f2<=0;
-    end
-  else
-    begin
-      if(ren && !empty)
-        begin
-          out<=mem[rptr[$clog2(WIDTH)-1:0]];
-         rptr<=rptr+1;
-          rptr1<=((rptr+1)>>1)^(rptr+1);
-        end
-          end
-    end
-  
- always @(posedge rclk )
-    begin
-  f1<=wptr1;
-  wptr2<=f1; 
- 
-end
+
     
+    // MEMORY
+
+    reg [7:0] mem [0:15];
+
+    
+    // POINTERS
+    
+
+    reg [4:0] wptr_bin;
+    reg [4:0] rptr_bin;
+
+    reg [4:0] wptr_gray;
+    reg [4:0] rptr_gray;
+
+    
+    // SYNCHRONIZER POINTERS
+    
+
+    reg [4:0] wptr_gray_sync1;
+    reg [4:0] wptr_gray_sync2;
+
+    reg [4:0] rptr_gray_sync1;
+    reg [4:0] rptr_gray_sync2;
+
+    
+    // WRITE LOGIC
   
-always@(posedge wclk )begin
-  f2<=rptr1;
-  rptr2<=f2; 
-end
-  
- always @(*)
-    begin
-  bin[$clog2(WIDTH)] = rptr2[$clog2(WIDTH)]; 
-        for (i = $clog2(WIDTH)-1; i >= 0; i = i - 1) begin
-          bin[i] = rptr2[i] ^ bin[i+1];
+
+    always @(posedge wclk or posedge rst) begin
+
+        if (rst) begin
+
+            wptr_bin  <= 5'b0;
+            wptr_gray <= 5'b0;
+
         end
- end
-  
-  always @(*)
-    begin
-      wbin[$clog2(WIDTH)] = wptr2[$clog2(WIDTH)]; 
-        for (i = $clog2(WIDTH)-1; i >= 0; i = i - 1) begin
-          wbin[i] = wptr2[i] ^ wbin[i+1];
+
+        else begin
+
+            if (wen && !full) begin
+
+                mem[wptr_bin[3:0]] <= data_in;
+
+                wptr_bin <= wptr_bin + 1'b1;
+
+                wptr_gray <= ((wptr_bin + 1'b1) >> 1)
+                             ^ (wptr_bin + 1'b1);
+
+            end
         end
- end
+    end
+
+    
+    // READ LOGIC
+    
+
+    always @(posedge rclk or posedge rst) begin
+
+        if (rst) begin
+
+            rptr_bin  <= 5'b0;
+            rptr_gray <= 5'b0;
+
+            data_out  <= 8'b0;
+
+        end
+
+        else begin
+
+            if (ren && !empty) begin
+
+                data_out <= mem[rptr_bin[3:0]];
+
+                rptr_bin <= rptr_bin + 1'b1;
+
+                rptr_gray <= ((rptr_bin + 1'b1) >> 1)
+                             ^ (rptr_bin + 1'b1);
+
+            end
+        end
+    end
+
   
-  assign full= ({~wptr[$clog2(WIDTH)], wptr[$clog2(WIDTH)-1:0]} == bin)?1:0;
+    // SYNCHRONIZE WRITE POINTER INTO READ DOMAIN
   
-  assign  empty=(wbin==rptr)?1:0;
-  
-endmodule 
+
+    always @(posedge rclk or posedge rst) begin
+
+        if (rst) begin
+
+            wptr_gray_sync1 <= 5'b0;
+            wptr_gray_sync2 <= 5'b0;
+
+        end
+
+        else begin
+
+            wptr_gray_sync1 <= wptr_gray;
+            wptr_gray_sync2 <= wptr_gray_sync1;
+
+        end
+    end
+
+    
+    // SYNCHRONIZE READ POINTER INTO WRITE DOMAIN
+    
+
+    always @(posedge wclk or posedge rst) begin
+
+        if (rst) begin
+
+            rptr_gray_sync1 <= 5'b0;
+            rptr_gray_sync2 <= 5'b0;
+
+        end
+
+        else begin
+
+            rptr_gray_sync1 <= rptr_gray;
+            rptr_gray_sync2 <= rptr_gray_sync1;
+
+        end
+    end
+
+    //----------------------------------------------------------
+    // EMPTY CONDITION
+    //----------------------------------------------------------
+
+    assign empty = (wptr_gray_sync2 == rptr_gray);
+
+    //----------------------------------------------------------
+    // FULL CONDITION
+    //----------------------------------------------------------
+
+    assign full =
+      ((((wptr_bin + 1'b1) >> 1)
+      ^  (wptr_bin + 1'b1))
+      ==
+      {~rptr_gray_sync2[4:3],
+        rptr_gray_sync2[2:0]});
+
+endmodule
